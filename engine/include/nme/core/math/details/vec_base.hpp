00b38cfc8f6e8c2b8209ae70108bb51acb4f9930
NiekMSoftware <niek+github@meletstudios.com>
2026-07-06 10:13:02 +0200

    refactor(math): introduced vec_base.hpp

---
+++ engine/include/nme/core/math/details/vec_base.hpp
@@ -0,0 +1,93 @@
+#pragma once
+#include <glm/fwd.hpp>
+
+namespace nme::math {
+/** @brief Base class of all vectors. */
+template<typename T, usize N>
+struct VectorBase {
+    constexpr VectorBase() noexcept;
+
+    T data[N];
+};
+
+template <typename T, usize N>
+constexpr VectorBase<T, N>::VectorBase() noexcept
+    : data{}
+{ }
+
+/** @brief Partial specialization for 2-component vectors. */
+template<typename T>
+struct VectorBase<T, 2> {
+    constexpr VectorBase() noexcept;
+
+    union {
+        struct {
+            T x, y;
+        };
+        struct {
+            T r, g;
+        };
+        struct {
+            T s, t;
+        };
+        struct {
+            T u, v;
+        };
+        T data[2];
+    };
+};
+
+template <typename T>
+constexpr VectorBase<T, 2>::VectorBase() noexcept
+    : data{}
+{ }
+
+/** @brief Partial specialization for 3-component vectors. */
+template<typename T>
+struct VectorBase<T, 3> {
+    constexpr VectorBase() noexcept;
+
+    union {
+        struct {
+            T x, y, z;
+        };
+        struct {
+            T r, g, b;
+        };
+        T data[3];
+    };
+};
+
+template <typename T>
+constexpr VectorBase<T, 3>::VectorBase() noexcept
+: data{}
+{ }
+
+/** @brief Partial specialization for 4-component vectors. */
+template<typename T>
+struct VectorBase<T, 4> {
+    constexpr VectorBase() noexcept;
+
+    union {
+        struct {
+            T x, y, z, w;
+        };
+        struct {
+            T r, g, b, a;
+        };
+        T data[4];
+    };
+};
+
+template <typename T>
+constexpr VectorBase<T, 4>::VectorBase() noexcept
+: data{}
+{ }
+
+#if defined(NME_SSE)
+
+// TODO: Create specialization for 4-component floating-point vector with SSE
+
+#endif
+
+}  // namespace nme::math
--- engine/include/nme/core/math/vec.hpp
+++ engine/include/nme/core/math/vec.hpp
@@ -4,6 +4,7 @@
 #include <type_traits>
 
 #include "nme/platform/types.h"
+#include "details/vec_base.hpp"
 
 namespace nme::math {
 
