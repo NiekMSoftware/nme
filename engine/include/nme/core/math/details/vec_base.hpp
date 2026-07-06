dca495e1cd65df4572b32a79bc6102faaac8c388
NiekMSoftware <niek+github@meletstudios.com>
2026-07-06 13:30:19 +0200

    feat(platform): added simd to vec_base for float4 types

--- engine/include/nme/core/math/details/vec_base.hpp
+++ engine/include/nme/core/math/details/vec_base.hpp
@@ -1,8 +1,10 @@
 #pragma once
 
+#include "nme/platform/platform.h"
 #include "nme/platform/types.h"
 
 namespace nme::math {
+
 /** @brief Base class of all vectors. */
 template<typename T, usize N>
 struct VectorBase {
@@ -85,9 +87,34 @@ constexpr VectorBase<T, 4>::VectorBase() noexcept
     : data{}
 { }
 
-#if defined(NME_SSE)
+#if defined(NME_SIMD_SSE)
 
-// TODO: Create specialization for 4-component floating-point vector with SSE
+template<>
+struct alignas(16) VectorBase<f32, 4> {
+    constexpr VectorBase() noexcept;
+    explicit constexpr VectorBase(__m128 v) noexcept;
+
+    union {
+        struct {
+            f32 x, y, z, w;
+        };
+        struct {
+            f32 r, g, b, a;
+        };
+        f32 data[4];
+        __m128 v;
+    };
+
+    explicit constexpr operator __m128() const noexcept;
+};
+constexpr VectorBase<float, 4>::VectorBase() noexcept
+    : data {}
+{ }
+constexpr VectorBase<float, 4>::VectorBase(__m128 v) noexcept
+    : v {v}
+{ }
+
+constexpr VectorBase<float, 4>::operator __m128() const noexcept { return v; }
 
 #endif
 
