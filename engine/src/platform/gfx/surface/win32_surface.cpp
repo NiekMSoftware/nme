47079326c8be5086f1d697bc3b170cc7790b254f
NiekMSoftware <niek+github@meletstudios.com>
2026-07-18 20:20:17 +0200

    feat(engine): stub Win32 surface implementation

--- engine/src/platform/gfx/surface/win32_surface.cpp
+++ engine/src/platform/gfx/surface/win32_surface.cpp
@@ -1,3 +1,24 @@
 //
 // Created by niek on 7/18/2026.
 //
+
+// TODO: Implement win32 surface
+
+#include "nme/platform/gfx/gfx.h"
+
+namespace nme::gfx {
+
+Surface createSurface(const WindowDesc*, GfxError* out_err) {
+    if (out_err) *out_err = GfxError::None;
+    return Surface{1};
+}
+void destroySurface(Surface) {}
+
+bool poll_event(Surface, Event*) { return false; }
+bool surface_should_close(Surface) { return false; }
+Extent2D surface_size(Surface) { return Extent2D{1280, 720}; }
+NativeHandle surface_native(Surface) { return {}; }
+
+}  // namespace gfx
+
+// EOF
