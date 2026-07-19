857c48cb1c255b75e3dc12db5520cc804b790b84
NiekMSoftware <niek+github@meletstudios.com>
2026-07-19 10:23:41 +0200

    feat(engine): add Vulkan swapchain stubs
    Introduce stub functions for Vulkan swapchain management (`create_swapchain`, `destroy_swapchain`, `swapchain_acquire`, etc.), laying groundwork for future implementation.

--- engine/src/platform/gfx/backend/vulkan/vk_swapchain.cpp
+++ engine/src/platform/gfx/backend/vulkan/vk_swapchain.cpp
@@ -1,3 +1,19 @@
-//
-// Created by niek on 7/18/2026.
-//
+#include "vk_common.h"
+
+#include "nme/platform/gfx/gfx.h"
+
+namespace nme::gfx {
+
+Swapchain create_swapchain(Device, Surface, const SwapchainDesc*, GfxError* out_err) {
+    if (out_err) *out_err = GfxError::None;
+    return {};
+}
+
+void     destroy_swapchain(Swapchain) {}                                      // TODO: views, swapchain, surface, sync
+Texture  swapchain_acquire(Swapchain) { return {}; }                       // TODO: vkAcquireNextImageKHR
+void     swapchain_present(Swapchain) {}                                      // TODO: vkQueuePresentKHR
+GfxError swapchain_resize(Swapchain, Extent2D) { return GfxError::Unknown; }  // TODO: recreate
+Format   swapchain_format(Swapchain) { return Format::BGRA8Srgb; }            // TODO
+Extent2D swapchain_extent(Swapchain) { return {}; }                        // TODO
+
+}  // namespace nme::gfx
