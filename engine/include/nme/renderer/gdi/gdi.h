c8c244cba7e18f84f1a10c60907d8889faa8db64
NiekMSoftware <niek+github@meletstudios.com>
2026-07-21 13:32:54 +0200

    feat(renderer): add `gdi.h` for GDI renderer interface definition

---
+++ engine/include/nme/renderer/gdi/gdi.h
@@ -0,0 +1,14 @@
+#ifndef NME_RENDERER_GDI_GDI_H_
+#define NME_RENDERER_GDI_GDI_H_
+
+#include "nme/platform/gfx/gfx.h"
+#include "nme/platform/memory/allocator.h"
+#include "nme/renderer/gdi/gdi_types.h"
+
+namespace nme::gdi {
+
+
+
+}  // namespace nme::gdi
+
+#endif  // NME_RENDERER_GDI_GDI_H_
