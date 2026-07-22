41a7b5886539a5ff654fda4de376d6465831fdaf
NiekMSoftware <niek+github@meletstudios.com>
2026-07-22 09:39:36 +0200

    add(filesystem): enhance `read_file.h` with file reading and memory management utilities
    Introduce `FileBlob` structure, `file_read_entire`, `file_read_into`, and `file_blob_free` functions for reading files and managing file data in memory.

--- engine/include/nme/platform/filesys/read_file.h
+++ engine/include/nme/platform/filesys/read_file.h
@@ -1,8 +1,28 @@
-//
-// Created by niek on 7/22/2026.
-//
+#ifndef NME_PLATFORM_FS_READ_FILE_H_
+#define NME_PLATFORM_FS_READ_FILE_H_
 
-#ifndef NME_READ_FILE_H
-#define NME_READ_FILE_H
+#include "nme/platform/error/result.h"
+#include "nme/platform/filesys/file.h"
+#include "nme/platform/types.h"
 
-#endif  // NME_READ_FILE_H
+// forward declare allocator
+namespace nme {
+struct Allocator;
+}  // namespace nme
+
+namespace nme::fs {
+
+// Bytes owned by the Allocator passed to file_read_entire; free via file_blob_free.
+struct FileBlob {
+    u8*   data;
+    usize size;
+};
+
+Result<FileBlob, FileError> file_read_into(const char* path, void* dst, usize cap);
+
+Result<FileBlob, FileError> file_read_entire(const char* path, const Allocator* allocator);
+void                        file_blob_free(FileBlob* blob, const Allocator* allocator);
+
+}  // namespace nme::fs
+
+#endif  // NME_PLATFORM_FS_READ_FILE_H_
