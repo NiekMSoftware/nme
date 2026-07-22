7ccff71e73bb45402c57dc4c830d543f909dbe94
NiekMSoftware <niek+github@meletstudios.com>
2026-07-22 16:57:11 +0200

    add(engine): extend `package.h` with `PackHeader` and `Package` structures
    Define `PackHeader` and `Package` structures to manage resource package metadata, file handling, and table of contents. Include new dependencies and update existing includes.

--- engine/include/nme/resource/package.h
+++ engine/include/nme/resource/package.h
@@ -1,6 +1,9 @@
 #ifndef NME_RESOURCE_PACKAGE_H_
 #define NME_RESOURCE_PACKAGE_H_
+
 #include "nme/core/string/string_id.h"
+#include "nme/platform/collections/dynamic_array.h"
+#include "nme/platform/filesys/file.h"
 #include "nme/platform/types.h"
 
 namespace nme::res {
@@ -31,9 +34,24 @@ struct PackEntry {
     u8              _pad[4];
 };
 
-struct PackHeader { };
+struct PackHeader {
+    u32 magic;
+    u32 version;
+    u32 flags;              // bit0: big-endian payload
+    u32 entry_count;
+    u64 toc_offset;         // PackEntry[entry_count]
+    u64 immediate_base;     // start of the immediate region
+    u64 bulk_base;          // start of the bulk region
+};
 
-struct Package { };
+struct Package {
+    fs::File                m_file;
+    DynamicArray<PackEntry> m_toc;      // TODO: swap lookup for the hash map
+    u64                     m_immediateBase;
+    u64                     m_bulkBase;
+    Allocator               m_alloc;
+    bool                    bOpen;
+};
 
 }  // namespace nme::res
 
